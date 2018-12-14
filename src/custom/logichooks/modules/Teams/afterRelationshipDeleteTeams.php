507f220531a94246891e56e9e9b16c995752b44e
Enrico Simonetti <enrico.simonetti@gmail.com>
2018-12-14 20:00:38 +1100

    Removed fatal log

--- src/custom/logichooks/modules/Teams/afterRelationshipDeleteTeams.php
+++ src/custom/logichooks/modules/Teams/afterRelationshipDeleteTeams.php
@@ -6,7 +6,6 @@ class afterRelationshipDeleteTeams
 {
     public function callAfterRelationshipDelete($b, $e, $a)
     {
-        $GLOBALS['log']->fatal('triggered callAfterRelationshipDelete');
         if ($a['module'] == 'Teams' && $a['related_module'] == 'Users') {
             // TODO optimise execution only when needed
             $oed = new OutboundEmailsDeployer();
